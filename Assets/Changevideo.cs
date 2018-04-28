27f811a3d94be6f9b7c59b6d469c060963153745
Nikhil <NIKHIL_SAWLANI@CRGL-THIRDPARTY.COM>
2018-04-28 09:52:32 +0530

    initial commit

---
+++ Assets/Changevideo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changevideo : MonoBehaviour {
+	public GameObject video1;
+	public GameObject video2;
+	public GameObject video3;
+	public GameObject video4;
+	// Use this for initialization
+	void Start () {
+		video1.SetActive (false);
+		video2.SetActive (false);
+		video3.SetActive (false);
+		video4.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	public void play1(){
+		video1.SetActive (true);
+		video2.SetActive (false);
+		video3.SetActive (false);
+		video4.SetActive (false);
+
+	}
+
+			public void play2(){
+		video1.SetActive (false);
+		video2.SetActive (true);
+		video3.SetActive (false);
+		video4.SetActive (false);
+
+	}
+
+	public void play3(){
+		video1.SetActive (false);
+		video2.SetActive (false);
+		video3.SetActive (true);
+		video4.SetActive (false);
+
+	}
+
+	public void play4(){
+		video1.SetActive (false);
+		video2.SetActive (false);
+		video3.SetActive (false);
+		video4.SetActive (true);
+
+	}
+}
---
+++ portal/Classes/Native/Il2CppCompilerCalculateTypeValues_20Table.cpp
@@ -0,0 +1,110 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+#include <stdint.h>
+
+#include "il2cpp-class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "il2cpp-object-internals.h"
+
+
+
+
+
+
+#ifndef RUNTIMEOBJECT_H
+#define RUNTIMEOBJECT_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.Object
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // RUNTIMEOBJECT_H
+#ifndef VALUETYPE_T3640485471_H
+#define VALUETYPE_T3640485471_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// System.ValueType
+struct  ValueType_t3640485471  : public RuntimeObject
+{
+public:
+
+public:
+};
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+// Native definition for P/Invoke marshalling of System.ValueType
+struct ValueType_t3640485471_marshaled_pinvoke
+{
+};
+// Native definition for COM marshalling of System.ValueType
+struct ValueType_t3640485471_marshaled_com
+{
+};
+#endif // VALUETYPE_T3640485471_H
+#ifndef U24ARRAYTYPEU3D24_T2467506693_H
+#define U24ARRAYTYPEU3D24_T2467506693_H
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+
+// <PrivateImplementationDetails>/$ArrayType=24
+#pragma pack(push, tp, 1)
+struct  U24ArrayTypeU3D24_t2467506693 
+{
+public:
+	union
+	{
+		struct
+		{
+		};
+		uint8_t U24ArrayTypeU3D24_t2467506693__padding[24];
+	};
+
+public:
+};
+#pragma pack(pop, tp)
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+#endif // U24ARRAYTYPEU3D24_T2467506693_H
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize2000 = { sizeof (U24ArrayTypeU3D24_t2467506693)+ sizeof (RuntimeObject), sizeof(U24ArrayTypeU3D24_t2467506693 ), 0, 0 };
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
