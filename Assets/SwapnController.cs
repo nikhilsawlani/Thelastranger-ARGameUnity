c306de104da6bbf03b31437d1afeb2a89fbf6e30
Nikhil <NIKHIL_SAWLANI@CRGL-THIRDPARTY.COM>
2018-04-29 00:01:57 +0530

    sphere added

--- Assets/SwapnController.cs
+++ Assets/SwapnController.cs
@@ -8,9 +8,12 @@ public class SwapnController : MonoBehaviour {
 
 	public GameObject zombie;
 	public Button startgame;
+	public GameObject hide;
 	// Use this for initialization
 	void Start () {
+		
 		startgame.onClick.AddListener (startInvoke);
+
 	}
 	// Update is called once per frame
 	void Update () {
@@ -20,12 +23,12 @@ public class SwapnController : MonoBehaviour {
 
 	void startInvoke(){
 
-		InvokeRepeating ("spawn", 0f, 5f);
-
+		InvokeRepeating ("spawn", 0f, 7f);
+		hide.SetActive (false);
 	}
 
 	void spawn(){
-		Vector3 position = new Vector3 (Random.Range(-10f,10f),Random.Range(-3f,3f),Random.Range(-10f,10f));
+		Vector3 position = new Vector3 (Random.Range(-8f,8f),Random.Range(-3f,3f),Random.Range(-8f,8f));
 		Instantiate (zombie,position,Quaternion.Euler(0,0,0));
 	}
 }
