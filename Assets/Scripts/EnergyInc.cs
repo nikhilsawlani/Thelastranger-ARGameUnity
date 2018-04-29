e9efa3251d9f8a27951ffa60c6066ec4d07ff60e
Nikhil <NIKHIL_SAWLANI@CRGL-THIRDPARTY.COM>
2018-04-29 08:46:04 +0530

    final commit

---
+++ Assets/Scripts/EnergyInc.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyInc : MonoBehaviour {
+
+
+	public Text resl;
+	public int encount = 0;
+	public bool gotenergy = false;
+	public AudioSource energysound;
+	public GameObject itself;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gotenergy) {
+			IncEn ();
+		}
+	}
+
+	void OnCollisionEnter (Collision col){
+		//Debug.Log ("Enter");
+		if((col.gameObject.tag == "MainCamera")){
+
+			Debug.Log ("Enterenergy");
+			gotenergy = true;
+
+		}
+	}
+
+
+
+	void OnCollisionExit (Collision col){
+
+		if((col.gameObject.tag == "MainCamera")){
+
+			Debug.Log ("Exitenergy");
+			gotenergy = false;
+		}
+	}
+
+	void IncEn(){
+
+		encount += 5;
+		resl.text = encount.ToString ();
+		energysound.Play ();
+		Destroy (itself, .2f);
+	}
+}
---
+++ Assets/missiondone.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class missiondone : MonoBehaviour {
+
+
+	public GameObject its;
+	public bool gotenergy = false;
+	public Text mission;
+  	// Use this for initialization
+	void Start () {
+		mission.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, 0, Time.deltaTime * 15);
+
+		if (gotenergy) {
+			IncEn ();
+		}
+	}
+
+	void OnCollisionEnter (Collision col){
+		//Debug.Log ("Enter");
+		if((col.gameObject.tag == "MainCamera")){
+
+			Debug.Log ("Enterenergy");
+			gotenergy = true;
+
+		}
+	}
+
+
+
+	void OnCollisionExit (Collision col){
+
+		if((col.gameObject.tag == "MainCamera")){
+
+			Debug.Log ("Exitenergy");
+			gotenergy = false;
+		}
+	}
+
+	void IncEn(){
+
+		//encount += 5;
+		//resl.text = encount.ToString ();
+		//energysound.Play ();
+		Destroy (its, .2f);
+		mission.gameObject.SetActive (true);
+	}
+}
